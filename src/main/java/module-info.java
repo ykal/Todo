2349989026cea7683757b06c8a99c2dda01a90b6
Kasim <se.kasim.ebrahim@gmail.com>
2021-03-04 12:09:54 -0600

    Include module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module edu.miu.cs401.todo {
     requires javafx.fxml;
     requires java.sql;
     requires dataaccess;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires java.desktop;
 
     opens edu.miu.cs401.todo to javafx.fxml;
     exports edu.miu.cs401.todo;
